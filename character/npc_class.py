da27d5c4acfa16b823c8325f7d1c4a57e209e6cf
Melsana <melsana@gmail.com>
2021-11-07 17:21:12 -0500

    Added comments to explain what the code is doing.

--- character/npc_class.py
+++ character/npc_class.py
@@ -1,10 +1,16 @@
 #! /usr/bin python3
 
+# It is considered best practice to put all imports at the top of file not mixed in or right before it is actually
+# needed, even though you technically could.
 import random
 
 # Written by Tish Soulliard November 2021
 
+# It is standard practice to write constants with all caps.  It indicates these values should never change inside the
+# code.  Also, if use a tuple (a list in () instead of []) it makes it 'immutable' which means it can't be altered.
+# You can't add to or remove items from the list.
 STATS = ("Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma")
+# This is a dictionary.  It contains Key=Value pairs.  The Key must be a unique string the value can be anything.
 SKILLS = {'Acrobatics': 'Dexterity',
           'Athletics': 'Strength',
           'Arcana': 'Intelligence',
@@ -32,11 +38,28 @@ HOBBIES = ('painting', 'writing', 'music', 'fishing', 'tinkering', 'research', '
 
 
 class Npc:
+    """
+    Npc Class
+    This is used to store all the information and methods associated with an npc.
+    """
     def __init__(self, name, race, profession):
+        """
+        When you call a class initiator you are creating an object.  In this case in order to create a npc object
+        you need to pass a name, race and profession (aka class - but class is a python key word so you can't use it
+        directly)
+
+        :param name: The name of the NPC
+        :param race: The race of the NPC
+        :param profession: The class of the NPC
+        """
         self.name = name
         self.race = race
         self.profession = profession
+        # in order to create some random stats we will call the populate_stats function.  This function does not need
+        # any information from this object so it is not a method associated with the class it is it's own function.
         self.stat = populate_stats()
+        # in order to create some random bonus stats we will call __populate_bonus.  This function does use values
+        # associated with the object, so it is a method of the class.
         self.bonus = self.__populate_bonus()
         self.armor_class = self.bonus['Dexterity'] + 8
         self.hit_points = self.bonus['Constitution'] + 8
@@ -46,28 +69,63 @@ class Npc:
         self.hobby = random.choice(HOBBIES)
 
     def __populate_bonus(self):
+        """
+        This will use the object's stats values to create some bonus stats.  This is a private method.  The __ at the
+        start of the  method name indicates this.  It means it should not be called by any code outside of the class.
+        It is just a naming convention and is not actually enforced by
+        python in any way.  __ (is called dunder).
+
+        :return: a dictonary containing the bonus stat values.
+        """
+        # create an empty dictionary
         bonus = {}
+        # cycle through each of the stats in the STATS tuple
         for stat in STATS:
+            # calculate a bonus stat value based on the provided base stat.
             bonus[stat] = (int(self.stat[stat]) // 2) - 5
         return bonus
 
     def __populate_skills(self):
+        """
+        This will use the object's bonus stat values to create some skill stats.  This is a private method.
+        The __ at the start of the  method name indicates this.  It means it should not be called by any code outside
+        of the class. It is just a naming convention and is not actually enforced by python in any way.
+        __ (is called dunder).
+
+        :return: a dictonary containing the skill stat values.
+        """
+        # create an empty dictionary
         skills = {}
+        # cycle through each of the skills in SKILLS dictionary.  Since we are iterating over the .items() list it
+        # provides a key and value pair at a time.
         for skill, stat in SKILLS.items():
+            # calculate a skill value based on the correct bonus stat as provided by the STATS dictonary.
             skills[skill] = random.randint(1, 20) + self.bonus[stat]
         return skills
 
     def __repr__(self):
+        """
+        This is a default/built in method that all classes have, but you can overwrite.  When you over write it, the
+        value you return is what will be used when you try to print the object, or try to represent it as a string.
+
+        :return: A string representing the object.
+        """
         output = f"Behold, {self.name} the {self.race} {self.profession} is here!\n"
+        # if you += a string it just appends the right string to the left string.
         output += "NPC stat modifiers "
+        # Cycling through each stat rather than typing them manually.
         for stat in STATS:
             sta = stat[0:3].upper()
             output += f"{sta} {self.bonus[stat]} | "
+        # In this case we don't want the last '| ' that is now on the string, so we are creating a substring that starts
+        # at the beginning of the output string and ends -2 spaces from the end.  In python the start number is assumed
+        # to be 0 if not declared.  Also, the end number is NOT included in the resulting string (or array).
         output = output[:-2]
         output += f"\nNatural AC is {self.armor_class} and HP is {self.hit_points}\n\n"
         output += f"{self.name} has a {self.appearance} appearance and looks to be {self.attitude}\n"
         output += f"{self.name}'s hobbies include {self.hobby}\n\n"
         output += "---Skill Checks---\n\n"
+        # Cycling through each of the skills in the SKILLS dictonary.  This time we don't need the value just the key.
         for skill in SKILLS.keys():
             output += f"{skill} check is {self.skills[skill]}\n"
         output += "\n"
@@ -75,6 +133,11 @@ class Npc:
 
 
 def populate_stats():
+    """
+    This function does not require explicit knowledge of the object so it is  not a part of the class definition.
+
+    :return: a dictonary containing random stats values.
+    """
     stat_value = {}
     for stat in STATS:
         stat_value[stat] = random.randint(5, 14)
--- npc_generator.py
+++ npc_generator.py
@@ -1,7 +1,11 @@
 #! /usr/bin python3
 
-from character import npc_class
+# the npc_class file that contains the Npc class I created is inside the character directory.  So character is the name
+# of the module and then npc_class is a feature of the module that you can import.  If you had just imported character
+# then when you try to create the npc object you would have to do it like this
+# npc = character.npc_class.Npc(char_name, char_race, char_class)
 
+from character import npc_class
 
 # Re-written by Tish Soulliard November 2021
 
@@ -9,25 +13,40 @@ from character import npc_class
 def main():
     print('NPC GENERATOR\n')
 
+    # Setting a boolean that will controll the while loop which is used for creating new npc objects.
     create_npc = True
+    # Creating an empty list to store all the npc objects that are created.
     npc_list = []
     while create_npc:
         char_name = input('NPC name? ')
         char_race = input('What race is the npc? ')
         char_class = input('What profession does the npc have? ')
 
+        # Creating the npc object based on the given name, race, and class
         npc = npc_class.Npc(char_name, char_race, char_class)
+        # Print to screen the npc object - this comes from the __repr__ method
         print(f"\n{npc}")
+        # Add this npc to the npc_list list, for use later if we want.
         npc_list.append(npc)
 
-        restart = input('Make another NPC? ').lower()
-        if not (restart == "yes" or restart == "y"):
+        make_another = input('Make another NPC? ').lower()
+        if not (make_another == "yes" or make_another == "y"):
+            # if make_another is not yes or y then create_npc will be set to False which will break us out of the while
+            # loop.
             create_npc = False
 
-    write = input('Would you like to write the characters to a file?').lower()
+    # I added this section to show you a few more things.
+    # Ask if you want to save the npc's you've created to a file.
+    write = input('Would you like to write the character(s) to a file?').lower()
     if write == "yes" or write == "y":
+        # If they do want to save, ask what filename.
         filename = input("Filename?")
+        # if you use with open, then when you break out of the with scope it will automatically close the file, you
+        # don't need to do it manually, so when possible this is considered a best practice.
+        # When python opens a file with 'w' it will overwrite any pre-existing file with that name.  I have not written
+        # any sort of check, so be careful what you name the file.
         with open(filename, "w") as outfile:
+            # cycle through each created npc object and write it to the file.
             for npc in npc_list:
                 outfile.write(f"{npc}")
 
